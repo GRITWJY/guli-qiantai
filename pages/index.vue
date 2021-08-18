16b611458e1ee2ef0c6e7194ea0c6ee202a8fc59
王蒋煜 <1435408225@qq.com>
2021-08-18 23:13:15 +0800

    首页banner

--- pages/index.vue
+++ pages/index.vue
@@ -27,23 +27,22 @@
           <div>
             <article class="comm-course-list">
               <ul class="of" id="bna">
-                <li>
+                <li v-for="course in eduList" :key="course.id">
                   <div class="cc-l-wrap">
                     <section class="course-img">
                       <img
-                        src="../assets/photo/course/1442295592705.jpg"
+                        :src="course.cover"
                         class="img-responsive"
-                        alt=""
                       >
                       <div class="cc-mask">
                         <a href="#" title="开始学习" class="comm-btn c-btn-1">开始学习</a>
                       </div>
                     </section>
                     <h3 class="hLh30 txtOf mt10">
-                      <a href="#" title="数学给宝宝带来兴趣" class="course-title fsize18 c-333">听力口语</a>
+                      <a href="#" :title="course.title" class="course-title fsize18 c-333">{{course.title}}</a>
                     </h3>
                     <section class="mt10 hLh20 of">
-                      <span class="fr jgTag bg-green">
+                      <span class="fr jgTag bg-green" v-if="Number(course.price) === 0">
                         <i class="c-fff fsize12 f-fA">免费</i>
                       </span>
                       <span class="fl jgAttr c-ccc f-fA">
@@ -54,88 +53,6 @@
                     </section>
                   </div>
                 </li>
-                <li>
-                  <div class="cc-l-wrap">
-                    <section class="course-img">
-                      <img
-                        src="../assets/photo/course/1442295592705.jpg"
-                        class="img-responsive"
-                        alt=""
-                      >
-                      <div class="cc-mask">
-                        <a href="#" title="开始学习" class="comm-btn c-btn-1">开始学习</a>
-                      </div>
-                    </section>
-                    <h3 class="hLh30 txtOf mt10">
-                      <a href="#" title="数学给宝宝带来兴趣" class="course-title fsize18 c-333">听力口语</a>
-                    </h3>
-                    <section class="mt10 hLh20 of">
-                      <span class="fr jgTag bg-green">
-                        <i class="c-fff fsize12 f-fA">免费</i>
-                      </span>
-                      <span class="fl jgAttr c-ccc f-fA">
-                        <i class="c-999 f-fA">9634人学习</i>
-                        |
-                        <i class="c-999 f-fA">9634评论</i>
-                      </span>
-                    </section>
-                  </div>
-                </li>
-                <li>
-                  <div class="cc-l-wrap">
-                    <section class="course-img">
-                      <img
-                        src="../assets/photo/course/1442295592705.jpg"
-                        class="img-responsive"
-                        alt=""
-                      >
-                      <div class="cc-mask">
-                        <a href="#" title="开始学习" class="comm-btn c-btn-1">开始学习</a>
-                      </div>
-                    </section>
-                    <h3 class="hLh30 txtOf mt10">
-                      <a href="#" title="数学给宝宝带来兴趣" class="course-title fsize18 c-333">听力口语</a>
-                    </h3>
-                    <section class="mt10 hLh20 of">
-                      <span class="fr jgTag bg-green">
-                        <i class="c-fff fsize12 f-fA">免费</i>
-                      </span>
-                      <span class="fl jgAttr c-ccc f-fA">
-                        <i class="c-999 f-fA">9634人学习</i>
-                        |
-                        <i class="c-999 f-fA">9634评论</i>
-                      </span>
-                    </section>
-                  </div>
-                </li>
-                <li>
-                  <div class="cc-l-wrap">
-                    <section class="course-img">
-                      <img
-                        src="../assets/photo/course/1442295592705.jpg"
-                        class="img-responsive"
-                        alt=""
-                      >
-                      <div class="cc-mask">
-                        <a href="#" title="开始学习" class="comm-btn c-btn-1">开始学习</a>
-                      </div>
-                    </section>
-                    <h3 class="hLh30 txtOf mt10">
-                      <a href="#" title="数学给宝宝带来兴趣" class="course-title fsize18 c-333">听力口语</a>
-                    </h3>
-                    <section class="mt10 hLh20 of">
-                      <span class="fr jgTag bg-green">
-                        <i class="c-fff fsize12 f-fA">免费</i>
-                      </span>
-                      <span class="fl jgAttr c-ccc f-fA">
-                        <i class="c-999 f-fA">9634人学习</i>
-                        |
-                        <i class="c-999 f-fA">9634评论</i>
-                      </span>
-                    </section>
-                  </div>
-                </li>
-
               </ul>
               <div class="clear"></div>
             </article>
@@ -157,83 +74,23 @@
           <div>
             <article class="i-teacher-list">
               <ul class="of">
-                <li>
+                <li v-for="teacher in teacherList" :key="teacher.id">
                   <section class="i-teach-wrap">
                     <div class="i-teach-pic">
-                      <a href="/teacher/1" title="王蒋煜">
-                        <img src="../assets/photo/teacher/1442297885942.jpg">
+                      <a href="/teacher/1" :title="teacher.name">
+                        <img :src="teacher.avatar">
                       </a>
                     </div>
                     <div class="mt10 hLh30 txtOf tac">
-                      <a href="/teacher/1" title="姚晨" class="fsize18 c-666">王蒋煜</a>
+                      <a href="/teacher/1" :title="teacher.title" class="fsize18 c-666">{{teacher.name}}</a>
                     </div>
                     <div class="hLh30 txtOf tac">
-                      <span class="fsize14 c-999">北京师范大学法学院副教授</span>
+                      <span class="fsize14 c-999">{{teacher.career}}</span>
                     </div>
                     <div class="mt15 i-q-txt">
                       <p
                         class="c-999 f-fA"
-                      >北京师范大学法学院副教授、清华大学法学博士。自2004年至今已有9年的司法考试培训经验。长期从事司法考试辅导，深知命题规律，了解解题技巧。内容把握准确，授课重点明确，层次分明，调理清晰，将法条法理与案例有机融合，强调综合，深入浅出。</p>
-                    </div>
-                  </section>
-                </li>
-                <li>
-                  <section class="i-teach-wrap">
-                    <div class="i-teach-pic">
-                      <a href="/teacher/1" title="王蒋煜">
-                        <img src="../assets/photo/teacher/1442297885942.jpg">
-                      </a>
-                    </div>
-                    <div class="mt10 hLh30 txtOf tac">
-                      <a href="/teacher/1" title="姚晨" class="fsize18 c-666">王蒋煜</a>
-                    </div>
-                    <div class="hLh30 txtOf tac">
-                      <span class="fsize14 c-999">北京师范大学法学院副教授</span>
-                    </div>
-                    <div class="mt15 i-q-txt">
-                      <p
-                        class="c-999 f-fA"
-                      >北京师范大学法学院副教授、清华大学法学博士。自2004年至今已有9年的司法考试培训经验。长期从事司法考试辅导，深知命题规律，了解解题技巧。内容把握准确，授课重点明确，层次分明，调理清晰，将法条法理与案例有机融合，强调综合，深入浅出。</p>
-                    </div>
-                  </section>
-                </li>
-                <li>
-                  <section class="i-teach-wrap">
-                    <div class="i-teach-pic">
-                      <a href="/teacher/1" title="王蒋煜">
-                        <img src="../assets/photo/teacher/1442297885942.jpg">
-                      </a>
-                    </div>
-                    <div class="mt10 hLh30 txtOf tac">
-                      <a href="/teacher/1" title="姚晨" class="fsize18 c-666">王蒋煜</a>
-                    </div>
-                    <div class="hLh30 txtOf tac">
-                      <span class="fsize14 c-999">北京师范大学法学院副教授</span>
-                    </div>
-                    <div class="mt15 i-q-txt">
-                      <p
-                        class="c-999 f-fA"
-                      >北京师范大学法学院副教授、清华大学法学博士。自2004年至今已有9年的司法考试培训经验。长期从事司法考试辅导，深知命题规律，了解解题技巧。内容把握准确，授课重点明确，层次分明，调理清晰，将法条法理与案例有机融合，强调综合，深入浅出。</p>
-                    </div>
-                  </section>
-                </li>
-                <li>
-                  <section class="i-teach-wrap">
-                    <div class="i-teach-pic">
-                      <a href="/teacher/1" title="王蒋煜">
-                        <img src="../assets/photo/teacher/1442297885942.jpg">
-                      </a>
-                    </div>
-                    <div class="mt10 hLh30 txtOf tac">
-                      <a href="/teacher/1" title="姚晨" class="fsize18 c-666">王蒋煜</a>
-                    </div>
-                    <div class="hLh30 txtOf tac">
-                      <span class="fsize14 c-999">北京师范大学法学院副教授</span>
-                    </div>
-                    <div class="mt15 i-q-txt">
-                      <p
-                        class="c-999 f-fA"
-                      >北京师范大学法学院副教授、清华大学法学博士。自2004年至今已有9年的司法考试培训经验。长期从事司法考试辅导，深知命题规律，了解解题技巧。内容把握准确，授课重点明确，层次分明，调理清晰，将法条法理与案例有机融合，强调综合，深入浅出。</p>
+                      >{{teacher.intro}}</p>
                     </div>
                   </section>
                 </li>
@@ -253,6 +110,7 @@
 </template>
 <script>
 import banner from "@/utils/banner";
+import indexApi from "@/utils/index"
 export default {
   data(){
     return{
@@ -267,6 +125,8 @@ export default {
         }
       },
       bannerList:[],
+      eduList:[],
+      teacherList:[]
     }
   },
   methods:{
@@ -277,9 +137,17 @@ export default {
       })
     },
 
+    getHot(){
+      indexApi.getIndexData().then(res=>{
+        this.eduList = res.data.data.eduList
+        this.teacherList = res.data.data.teacherList
+      })
+    }
+
   },
   created() {
     this.getBannerList()
+    this.getHot()
   }
 }
 </script>
---
+++ utils/index.js
@@ -0,0 +1,16 @@
+import request from "@/utils/request";
+
+
+export default {
+	getIndexData(){
+		return request({
+			url:'/eduservice/indexfront/index',
+			method:'get'
+		})
+	}
+}
+
+
+
+
+
